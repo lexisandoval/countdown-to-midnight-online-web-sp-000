5d4b0f7822dd24ef2ec4bda067a2d105d47b872d
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-13 20:17:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
 def countdown(integer)
-  number = integer
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
   end
   puts "HAPPY NEW YEAR!"
   return "HAPPY NEW YEAR!"
